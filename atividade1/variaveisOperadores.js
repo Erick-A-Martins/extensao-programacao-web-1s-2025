bb967de19ce49285013289d05e3fed5f76ee43c0
Erick-A-Martins <erickalvesqmartins@gmail.com>
2025-02-28 10:47:16 -0300

    done atividade1

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,56 @@
+// console.log('Hello, world!');
+
+// const n = 1;
+
+// console.log('n = ' + n);
+
+// let j = 3;
+// j = 4;
+
+// let texto = "Meu texto";
+// texto = 'Meu texto';
+
+// let nome = "Erick";
+// let telefone = '61 0000-0000';
+
+// console.log(`Meu nome é ${nome}. Telefone: ${telefone}`);
+
+// let x = 3.14;
+
+// x = 0.5;
+// x = 1;
+
+// let b = false;
+// b = true;
+
+// let c = -1;
+
+// let conexao;
+
+// console.log(`conexao = ${conexao}`);
+
+
+// EXERCICIOS 1 E 2
+// Soma
+let soma_A = 5, soma_B = 3, resultadoSoma;
+resultadoSoma = soma_A + soma_B;
+
+console.log(`Resultado da soma entre ${soma_A} e ${soma_B} é igual a: ${resultadoSoma}`);
+
+// Subtração
+let sub_A = 20, sub_B = 12, resultadoSub;
+resultadoSub = sub_A - sub_B;
+
+console.log(`Resultado da subtração entre ${sub_A} e ${sub_B} é igual a: ${resultadoSub}`);
+
+// Multiplicação
+let mul_A = 7, mul_B = 5, resultadoMul;
+resultadoMul = mul_A * mul_B;
+
+console.log(`Resultado da multiplicação entre ${mul_A} e ${mul_B} é igual a: ${resultadoMul}`);
+
+// Divisão
+let div_A = 15, div_B = 3, resultadoDiv;
+resultadoDiv = div_A / div_B;
+
+console.log(`Resultado da divisão entre ${div_A} e ${div_B} é igual a: ${resultadoDiv}`);
