c5af04b765c00387a3ad5f9ae2a9c40dc7cabd30
Erick Martins <erickalvesqmartins@gmail.com>
2025-05-23 21:30:29 -0300

    done atividade5

--- atividade5-example/index.js
+++ atividade5-example/index.js
@@ -5,10 +5,11 @@ const app = express();
 app.engine('html', mustacheExpress());
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
-app.use(express.urlencoded({extended: true}));
 
-const agendamentoRouter = require('./routers/agendamentoRouter');
-app.use('/', agendamentoRouter);
+app.get('/ola/:nome', (req, res) => {
+    let nome = req.params.nome;
+    res.render('index.html', {nome});
+});
 
 const PORT = 8080;
 app.listen(PORT, () => {
